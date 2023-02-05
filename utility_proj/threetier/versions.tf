b41338de785ca093d9c04b89613a1c29a5ac3cec
Mahesh Y <maheshy04@gmail.com>
2023-02-05 17:13:47 +0530

    threetier

---
+++ utility_proj/threetier/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  //required_version = "3.18"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+
+    }
+  }
+}
+provider "aws" {
+  region  = "ap-south-1"
+  profile = "mahesh"
+}
