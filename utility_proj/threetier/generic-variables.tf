fa56c52d1dbc8e2c177a88d871c060e9b2488c5f
Mahesh Y <maheshy04@gmail.com>
2023-02-24 12:26:44 +0530

    threetier

--- utility_proj/threetier/generic-variables.tf
+++ utility_proj/threetier/generic-variables.tf
@@ -7,15 +7,26 @@ variable "aws_region" {
 }
 
 # AWS EC2 Instance Type
-variable "instance_type" {
-  description = "EC2 Instance Type"
+variable "bastion_instance_type" {
+  description = "EC2 Instance Type for Bastion"
   type        = string
-  default     = "t2.micro"
+  default     = "t2.small"
 }
 
+variable "instance_type" {
+  description = "EC2 instance for workloads"
+  type = string
+  default = "t2.micro"
+}
 # AWS EC2 Instance Key Pair
 variable "instance_keypair" {
   description = "AWS EC2 Key pair that need to be associated with EC2 Instance"
   type        = string
   default     = "clouddirty"
+}
+
+variable "private_instance_count" {
+  description = "Instance count for private subnet"
+  type = number
+  default = 2
 }
