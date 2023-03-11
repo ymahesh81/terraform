d9ffae20e4ce865d29640b4a6809a96f12732177
Mahesh Y <maheshy04@gmail.com>
2023-03-11 17:33:08 +0530

    modifiedoutput

--- utility_proj/threetier/ec2_inst_output.tf
+++ utility_proj/threetier/ec2_inst_output.tf
@@ -1,24 +1,34 @@
-#public instance - bastion IP
-output "ec2_bastion_ids" {
-  description = "List of IDs of instances"
-  value       = module.ec2_bastion.id
+# #public instance - bastion IP
+# output "ec2_bastion_ids" {
+#   description = "List of IDs of instances"
+#   value       = module.ec2_bastion.id
+# }
+
+
+# output "ec2_bastion_public_dns" {
+#   description = "List of public IP assigned to the instances"
+#   value       = module.ec2_bastion.public_ip
+# }
+
+# #private instance IP
+
+# output "ec2_private_ids" {
+#   description = "List of IDs of instances"
+#   value       = module.ec2_private.id
+# }
+
+
+# output "ec2_private_public_dns" {
+#   description = "List of public IP assigned to the instances"
+#   value       = module.ec2_private.public_ip
+# }
+
+output "id" {
+  description = "The ID of the instance"
+  value       = [module.ec2_bastion.id]
 }
 
-
-output "ec2_bastion_public_dns" {
-  description = "List of public IP assigned to the instances"
-  value       = module.ec2_bastion[*].public_ip
-}
-
-#private instance IP
-
-output "ec2_private_ids" {
-  description = "List of IDs of instances"
-  value       = module.ec2_private[*].id
-}
-
-
-output "ec2_private_public_dns" {
-  description = "List of public IP assigned to the instances"
-  value       = module.ec2_private[*].public_ip
-}
+# output "arn" {
+#   description = "The ARN of the instance"
+#   value       = try(aws_instance.this[0].arn)
+# }
--- utility_proj/threetier/ec2_private.tf
+++ utility_proj/threetier/ec2_private.tf
@@ -1,6 +1,6 @@
 module "ec2_private" {
   source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "2.17.0"
+  version = "~> 3.0"
   depends_on = [ module.vpc ]
  
   # name = "${local.environment}-vm"
@@ -15,9 +15,10 @@ module "ec2_private" {
   count = var.private_instance_count
   key_name = var.instance_keypair
   
-  subnet_ids = [
-    module.vpc.private_subnets[0],
-    module.vpc.private_subnets[1]
-  ]
+  # subnet_id = [
+  #   module.vpc.private_subnets[0],
+  #   module.vpc.private_subnets[1]
+  # ]
+  subnet_id = module.vpc.private_subnets[count.index]
  tags = local.common_tags
 }
